41246d6ce06e9f6764498888fd82ceb216b1243a
kuco23 <nseverkar@gmail.com>
2022-03-05 21:02:22 +0100

    simplified

--- medium_code.py
+++ medium_code.py
@@ -7,22 +7,16 @@ from matplotlib import cm
 from matplotlib.colors import Normalize
 
 
-def mapToComplexPlane(n, a, i, j):
-    return complex(2 * a * j / n - a, 2 * a * i / n - a)
-
-def mapToComplexPlaneCenter(n, c, r, i, j):
-    return -c + r * complex(2 * j / n - 1, 2 * i / n - 1)
-
+# evaluates the polynomial p at point z
 def horner(p, z):
     return reduce(lambda x, y: x * z + y, p)
 
-def differentiate(poly):
-    n, an = len(poly) - 1, poly[0]
-    return [(n - i) * an for (i, an) in enumerate(poly[:-1])]
-
-def inCardioidOrCircle(c):
-    s = cmath.sqrt(1 - 4 * c)
-    return abs(1 + s) <= 1 or abs(1 - s) <= 1 or abs(1 + c) <= 1/4
+def escapetime(p, z, R, K):
+    k, zk = 1, z
+    while k < K and abs(zk) <= R:
+        zk = horner(p, zk)
+        k += 1
+    return k
 
 def drawPPM(filename, rgbfun, n):
     with open(filename, 'a') as _: pass
@@ -40,18 +34,13 @@ def drawPPMCircle(n, c, r):
         return '0 0 0' if radius < r else '255 255 255'
     drawPPM('circle.ppm', rgbcircle, n)
 
-def escapetime(p, z, R, K):
-    k, zk = 1, z
-    while k < K and abs(zk) <= R:
-        zk = horner(p, zk)
-        k += 1
-    return k
+def mapToComplexPlaneCenter(n, c, r, i, j):
+    return c + r * complex(2 * j / n - 1, 2 * i / n - 1)
 
 def drawEscapetimeMandelbrot(n, ctr, r, colormap, K):
     p = lambda c: [1, 0, c]
     def rgbMandelbrot(i, j):
         c = mapToComplexPlaneCenter(n, ctr, r, i, j)
-        if inCardioidOrCircle(c): return '0 0 0'
         k = escapetime(p(c), 0, 2, K)
         if k == K: return '0 0 0'
         else:
@@ -63,23 +52,22 @@ def radiusJulia(poly, L):
     n = len(poly) - 1
     an = abs(poly[0])
     C = sum(map(abs, poly)) - an
-    eps = max(1, 2 * C / 2, pow(2 * L / an, 1 / (n-1)))
-    return eps
+    return max(1, 2 * C / 2, pow(2 * L / an, 1 / (n-1)))
 
 def drawEscapetimeJulia(n, p, colormap, K):
     rp = radiusJulia(p, 1.000001)
     def rgbJulia(i, j):
-        z = mapToComplexPlane(n, rp, i, j)
+        z = mapToComplexPlaneCenter(n, 0, rp, i, j)
         k = escapetime(p, z, rp, K)
         if k == K: return '0 0 0'
         else:
             cmap = colormap(k / K)[:3]
             return ' '.join(str(round(255 * cm)) for cm in cmap)
     drawPPM('escapetime_julia.ppm', rgbJulia, n)
-    
-def demMandelbrot(c, lim, overflow):
+
+def demMandelbrot(c, K, overflow):
     ck, dk = c, 1
-    for _ in range(lim):
+    for _ in range(K):
         if max(
             abs(ck.real), abs(ck.imag),
             abs(dk.real), abs(dk.imag)
@@ -91,20 +79,20 @@ def demMandelbrot(c, lim, overflow):
     if absck <= 2: return 0
     else:
         absdk = abs(dk)
-        if absdk == 0: return -1
+        if absdk == 0: return -1 # this will probably never happen
         estimate = log2(absck) * absck / absdk
         return -log2(estimate)
 
 def drawDemMandelbrot(n, ctr, r, colormap, K, overflow):
     arr = zeros((n, n), dtype=float)
-
+    
     for i in range(n):
         for j in range(n):
             c = mapToComplexPlaneCenter(n, ctr, r, i, j)
             arr[i,j] = demMandelbrot(c, K, overflow)
 
     m, M = arr.min(), arr.max()
-    arr[arr == 0] = M
+    arr[arr == 0] = M # 0 only denotes the inner set and it could spoil our normalization
     colortable = colormap(Normalize(m, M)(arr))
             
     def rgbMandelbrot(i, j):
@@ -114,9 +102,14 @@ def drawDemMandelbrot(n, ctr, r, colormap, K, overflow):
 
     drawPPM('demMandelbrot.ppm', rgbMandelbrot, n)
 
-def demJulia(p, dp, z, lim, overflow, R):
+# derivative of the given polynomial
+def differentiate(poly):
+    n, an = len(poly) - 1, poly[0]
+    return [(n - i) * an for (i, an) in enumerate(poly[:-1])]
+
+def demJulia(p, dp, z, K, overflow, R):
     zk, dk = z, 1
-    for _ in range(lim):
+    for _ in range(K):
         if max(
             abs(zk.real), abs(zk.imag),
             abs(dk.real), abs(dk.imag)
@@ -138,7 +131,7 @@ def drawDemJulia(n, p, colormap, K, overflow):
 
     for i in range(n):
         for j in range(n):
-            z = mapToComplexPlane(n, r, i, j)
+            z = mapToComplexPlaneCenter(n, 0, r, i, j)
             arr[i,j] = demJulia(p, dp, z, K, overflow, r)
 
     m, M = arr.min(), arr.max()
@@ -153,16 +146,18 @@ def drawDemJulia(n, p, colormap, K, overflow):
         return ' '.join(str(round(255 * cm)) for cm in cmap)
 
     drawPPM('demJulia.ppm', rgbJulia, n)
-            
+    
+
+def inCardioidOrCircle(c):
+    s = cmath.sqrt(1 - 4 * c)
+    return abs(1 + s) <= 1 or abs(1 - s) <= 1 or abs(1 + c) <= 1/4
+
 
 if __name__ == '__main__':
 
     drawDemMandelbrot(
-        2000, 0.8, 1.4,
+        2000, -0.8, 1.4,
         cm.get_cmap('gist_stern').reversed(),
         100, 10**20
     )
-    
-    
-    
     
